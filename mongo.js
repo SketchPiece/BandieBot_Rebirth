2c7dc8bd60724091ef61d89cfe3524fd2d7087c2
Your Name <your_email>
2020-01-04 11:34:53 +0300

    Какие то проверки. Ничего значительного

--- mongo.js
+++ mongo.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const uri = require("./botconfig.json").mongoose_uri
 
 mongoose.connect("mongodb://localhost:27017/bandie", { useNewUrlParser: true, useUnifiedTopology: true });
 
